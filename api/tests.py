c555e31a674d73e7532a1f5b11070e057bdcb444
dgchen <dgchen@thoughtworks.com>
2017-07-19 16:58:25 +0800

    [diangui] add example

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+class ApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient();
+        self.base_url = 'http://127.0.0.1:8000/'
+
+    def should_get_thing_shadow(self):
+        response = self.client.get(self.base_url+'things');
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
